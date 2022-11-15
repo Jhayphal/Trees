4306659cb426a1938787debaba21efc5b77550ea
Oleg Nazarenko <olegn@devart.com>
2022-11-16 00:22:47 +0200

    Refactoring.

--- Trees/Builders/PrefixTreeBuilder.cs
+++ Trees/Builders/PrefixTreeBuilder.cs
@@ -1,62 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Trees.Builders
+﻿namespace Trees.Builders
 {
-  public class PrefixTreeBuilder<TSource, TValue> :
-    ITreeBuilder<TSource, string, TValue>,
-    IForestBuilder<TSource, string, TValue>
+  public class PrefixTreeBuilder<TSource, TValue> : RecordSetTreeBuilder<TSource, string, TValue>
   {
-    public IEnumerable<INode<string, TValue>> BuildForest(IRecordSet<TSource, string, TValue> recordSet)
-    {
-      INode<string, TValue> root;
-      while ((root = Build(recordSet)) != null)
-      {
-        yield return root;
-      }
-    }
-
-    public INode<string, TValue> Build(IRecordSet<TSource, string, TValue> recordSet)
-    {
-      if (recordSet == null)
-      {
-        throw new ArgumentNullException(nameof(recordSet));
-      }
-
-      if (recordSet.EOF)
-      {
-        return null;
-      }
-
-      Node<string, TValue> root = Read(recordSet);
-      recordSet.MoveNext();
-      Walk(root, recordSet);
-      return root;
-    }
-
-    protected virtual bool IsChild(string parent, string maybeChild)
+    protected override bool IsChild(string parent, string maybeChild)
       => maybeChild?.StartsWith(parent) ?? false;
-
-    private bool IsChild(Node<string, TValue> parent, Node<string, TValue> maybeChild)
-      => IsChild(parent?.Key, maybeChild?.Key);
-
-    private void Walk(Node<string, TValue> parent, IRecordSet<TSource, string, TValue> recordSet)
-    {
-      if (recordSet.EOF)
-      {
-        return;
-      }
-
-      Node<string, TValue> maybeChild;
-      while (IsChild(parent, maybeChild = Read(recordSet)))
-      {
-        parent.AddChild(maybeChild);
-        recordSet.MoveNext();
-        Walk(maybeChild, recordSet);
-      }
-    }
-
-    private static Node<string, TValue> Read(IRecordSet<TSource, string, TValue> recordSet)
-      => new Node<string, TValue>(recordSet.ReadKey(), recordSet.ReadValue());
   }
 }
---
+++ Trees/Builders/RecordSetTreeBuilder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Trees.Builders
+{
+  public abstract class RecordSetTreeBuilder<TSource, TKey, TValue> : ITreeBuilder<TSource, TKey, TValue>,
+    IForestBuilder<TSource, TKey, TValue>
+    where TKey : IEquatable<TKey>, IComparable<TKey>
+  {
+    public IEnumerable<INode<TKey, TValue>> BuildForest(IRecordSet<TSource, TKey, TValue> recordSet)
+    {
+      INode<TKey, TValue> root;
+      while ((root = Build(recordSet)) != null)
+      {
+        yield return root;
+      }
+    }
+
+    public INode<TKey, TValue> Build(IRecordSet<TSource, TKey, TValue> recordSet)
+    {
+      if (recordSet == null)
+      {
+        throw new ArgumentNullException(nameof(recordSet));
+      }
+
+      if (recordSet.EOF)
+      {
+        return null;
+      }
+
+      Node<TKey, TValue> root = Read(recordSet);
+      recordSet.MoveNext();
+      Walk(root, recordSet);
+      return root;
+    }
+
+    protected abstract bool IsChild(TKey parent, TKey maybeChild);
+
+    private bool IsChild(Node<TKey, TValue> parent, Node<TKey, TValue> maybeChild)
+      => IsChild(parent.Key, maybeChild.Key);
+
+    private void Walk(Node<TKey, TValue> parent, IRecordSet<TSource, TKey, TValue> recordSet)
+    {
+      if (recordSet.EOF)
+      {
+        return;
+      }
+
+      Node<TKey, TValue> maybeChild;
+      while (IsChild(parent, maybeChild = Read(recordSet)))
+      {
+        parent.AddChild(maybeChild);
+        recordSet.MoveNext();
+        Walk(maybeChild, recordSet);
+      }
+    }
+
+    private static Node<TKey, TValue> Read(IRecordSet<TSource, TKey, TValue> recordSet)
+      => new Node<TKey, TValue>(recordSet.ReadKey(), recordSet.ReadValue());
+  }
+}
